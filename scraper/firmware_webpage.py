6089c31d128b280b4cdae4ccd8c127b2cf021468
saul.burgos <burgoscervantes@hotmail.com>
2023-08-17 12:10:27 -0600

    creating node html for firmwares

--- scraper/firmware_webpage.py
+++ scraper/firmware_webpage.py
@@ -1,12 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from bs4 import BeautifulSoup
 from typing import Dict, List
 
 from models import firmware
 
 @dataclass()
+class FirmwareNodeHTML:
+    html: BeautifulSoup
+
+@dataclass()
 class FirmwareWebPage:
-    html: BeautifulSoup = None  
+    html: BeautifulSoup = None
+    firmware_nodes: List[FirmwareNodeHTML] = field(default_factory=lambda: [])
 
     def get_firmware_list(self) -> List[Dict]:
         firmware_list = []
@@ -14,11 +19,13 @@ class FirmwareWebPage:
         children = self.get_direct_children_from_parent(self.get_parent_container())
 
         for child in children:
-            print(child)
+            self.firmware_nodes.append(FirmwareNodeHTML(html=child))
         
         return "I am a dictionary of firmware"
     
-    def get_direct_children_from_parent(self, parent_element: BeautifulSoup) -> List:
+    def get_direct_children_from_parent(
+        self, parent_element: BeautifulSoup
+    ) -> BeautifulSoup | List:
         return parent_element.find('div','view-content').find_all(recursive=False)
     
     def get_parent_container(self) -> BeautifulSoup | List:
